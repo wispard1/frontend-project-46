a14862952ee0e7768bc0049eaa0709eabe9c55f4
wispard1 <antonvlasov610@gmail.com>
2025-05-21 18:15:40 +0800

    update bin/index.js

--- bin/index.js
+++ bin/index.js
@@ -9,7 +9,7 @@ program
   .argument('<filepath1>')
   .argument('<filepath2>')
   .description('Compares two configuration files and shows a difference.')
-  // .option('-V, --version', 'output the version number')
+  .option('-V, --version', 'output the version number')
   .option('-f, --format [type]', 'output format', 'stylish')
   .action((file1, file2, { format }) => {
     const result = gendiff(file1, file2, format);
