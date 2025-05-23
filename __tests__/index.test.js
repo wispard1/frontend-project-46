c64e16aa318e8f693f05c90e3bd89a2e3597ae27
wispard1 <antonvlasov610@gmail.com>
2025-05-23 13:07:51 +0800

    Move tests to root directory

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -2,15 +2,15 @@ import fs from 'fs';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 import path from 'path';
-import gendiff from '../../src/index.js';
+import genDiff from '../index.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const getFixturePath = (filename) => path.join(__dirname, '..', filename);
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-describe('gendiff', () => {
+describe('genDiff', () => {
   let expectedStylish;
   let expectedPlain;
   let expectedJson;
@@ -18,43 +18,42 @@ describe('gendiff', () => {
   beforeAll(() => {
     expectedStylish = readFile('expectedStylish.txt').trim();
     expectedPlain = readFile('expectedPlain.txt').trim();
-
     expectedJson = JSON.parse(readFile('expectedJson.txt'));
   });
 
   test('should work with JSON format in stylish format', () => {
     const filepath1 = getFixturePath('file1.json');
     const filepath2 = getFixturePath('file2.json');
-    expect(gendiff(filepath1, filepath2, 'stylish')).toEqual(expectedStylish);
+    expect(genDiff(filepath1, filepath2, 'stylish')).toEqual(expectedStylish);
   });
 
   test('should work with JSON format in plain format', () => {
     const filepath1 = getFixturePath('file1.json');
     const filepath2 = getFixturePath('file2.json');
-    expect(gendiff(filepath1, filepath2, 'plain')).toEqual(expectedPlain);
+    expect(genDiff(filepath1, filepath2, 'plain')).toEqual(expectedPlain);
   });
 
   test('should work with YAML format in stylish format', () => {
     const filepath1 = getFixturePath('file1.yml');
     const filepath2 = getFixturePath('file2.yml');
-    expect(gendiff(filepath1, filepath2, 'stylish')).toEqual(expectedStylish);
+    expect(genDiff(filepath1, filepath2, 'stylish')).toEqual(expectedStylish);
   });
 
   test('should work with YAML format in plain format', () => {
     const filepath1 = getFixturePath('file1.yml');
     const filepath2 = getFixturePath('file2.yml');
-    expect(gendiff(filepath1, filepath2, 'plain')).toEqual(expectedPlain);
+    expect(genDiff(filepath1, filepath2, 'plain')).toEqual(expectedPlain);
   });
 
   test('should work with JSON format in json format', () => {
     const filepath1 = getFixturePath('file1.json');
     const filepath2 = getFixturePath('file2.json');
-    expect(JSON.parse(gendiff(filepath1, filepath2, 'json'))).toEqual(expectedJson);
+    expect(JSON.parse(genDiff(filepath1, filepath2, 'json'))).toEqual(expectedJson);
   });
 
   test('should throw error with unknown format', () => {
     const filepath1 = getFixturePath('file1.json');
     const filepath2 = getFixturePath('file2.json');
-    expect(() => gendiff(filepath1, filepath2, 'unknown')).toThrow('Unknown format: unknown');
+    expect(() => genDiff(filepath1, filepath2, 'unknown')).toThrow('Unknown format: unknown');
   });
 });
