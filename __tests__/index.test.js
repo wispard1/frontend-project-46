81c25a8d45e6a86731552ba877cdc0c72490b958
wispard1 <antonvlasov610@gmail.com>
2025-05-23 18:05:11 +0800

    Fix eslint issues

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -7,8 +7,9 @@ import genDiff from '../index.js'
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 
-const fixturesDir = path.join(__dirname, '..', '__fixtures__')
 const getFixturePath = (filename) => path.join(fixturesDir, filename)
+const readFile = (filename) =>
+  fs.readFileSync(getFixturePath(filename), 'utf-8')
 
 describe('genDiff', () => {
   let expectedStylish
