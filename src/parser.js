63a0b1e6edcdbd81e45f2f11933c29ad14dc213c
wispard1 <antonvlasov610@gmail.com>
2025-05-30 13:35:15 +0800

    update parser.js

--- src/parser.js
+++ src/parser.js
@@ -1,13 +1,17 @@
 import { parse } from 'yaml'
 
-export default function parser(data, ext) {
-  switch (ext) {
+export default function parser(data, format) {
+  if (typeof data !== 'string') {
+    throw new TypeError('Data must be a string')
+  }
+
+  switch (format) {
     case 'json':
       return JSON.parse(data)
     case 'yaml':
     case 'yml':
       return parse(data)
     default:
-      throw new Error(`Unsupported file extension '${ext}'`)
+      throw new Error(`Unsupported format: ${format}`)
   }
 }
